732e929ab2b8b8a945f881d7f2fc1981d8ba6f9e
jeancarlos96 <jeancarloschamba@hotmail.com>
2016-05-18 16:57:50 -0500

    TRADUCCION-Modificacion
    Hice que se imprimiera solo una vez la traducción.

--- tex.py
+++ tex.py
@@ -1,8 +1,7 @@
 from  textblob import TextBlob
 text = '''La historia empieza cuando Harry es dejado en la casa de sus Tios, los Dursley, por Rubeus Hagrid, Albus Dumbledore y Minerva McGonnagall.
 Alli mencionan que Lord Voldemort, el mago mas tenebroso de esos tiempos habia sido vencido, luego de intentar matar a Harry Potter.
-Harry sobrevivio a la maldicion asesina, con no otra secuela que una cicatriz con forma de rayo en la frente.'''
-text1 = '''Poco antes de su cumpleanios Harry recibe una carta de Hogwarts, pero no logra abrirla o causa de su tio,
+Harry sobrevivio a la maldicion asesina, con no otra secuela que una cicatriz con forma de rayo en la frente. Poco antes de su cumpleanios Harry recibe una carta de Hogwarts, pero no logra abrirla o causa de su tio,
 que no quiere que el chico se entere de que puede hacer magia. Los esfuerzos de Vernon no prosperan, y siguen llegando cartas, traidas extraniamente por lechuzas.
 Para que no haya mas disturbios con las cartas, la familia Dursley con Harry se mudan a una casita en una islita. El dia de cumpleanios de Harry,
 todavia en esa casita, llega el gigantesco Hagrid a entregarle la carta personalmente, y a desearle feliz cumpleaños. Vernon se tiene que dar por vencido,
@@ -14,15 +13,19 @@ Harry compra una varita, libros, y Hagrid le regala a Hedwig, una lechuza. Luego
 Gryffindor, Slytherin, Ravenclaw y Hufflepuff, y que va a ser asignador a una. Ron Weasley, un chico que habia conocido en el tren que los llevaba al Colegio,
 le dice que en Slytherin estaban los magos malos, y Harry desea no entrar a esa casa. Finalmente termina con Ronald en Gryffindor, 
 y el que acaba en Slytherin es Draco Malfoy, un chico que habia insultado a Ron.'''
+
+## ASI EVITAMOS QUE SE IMPRIMA VARIAS VECES EL TEXTO TRADUCIDO
 blob1 = TextBlob(text)
-blob2 = TextBlob(text1)
+##blob2 = TextBlob(text1)
 blob1.tags
-blob2.tags
+##blob2.tags
 blob1.noun_phrases
-blob2.noun_phrases
+##blob2.noun_phrases
+##for sentence in blob1.sentences:
+print ("#############-------- TRADUCCION ----##########################")
+print (blob1.translate (to = "en")) ##"en" en esta parte se pone el idionma al que se quiere traducir
 for sentence in blob1.sentences:
 	print(sentence.sentiment.polarity)
-	print (blob1.translate (to = "en"))
-for sentence in blob2.sentences:
-	print(sentence.sentiment.polarity)
-	print (blob2.translate (to = "en"))
+##for sentence in blob2.sentences:
+##	print(sentence.sentiment.polarity)
+##	print (blob2.translate (to = "en"))
